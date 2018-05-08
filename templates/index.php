d0d6d05606ea4804d1d46e39f5121fbe6700aedc
MarinaLik <mylym79@gmail.com>
2018-05-08 20:43:02 +0300

    Добавлено в верстку время лота, вставлена функция фильтрации текста в наименование лота

--- templates/index.php
+++ templates/index.php
@@ -22,14 +22,14 @@
             </div>
             <div class="lot__info">
                 <span class="lot__category"><?=$lot['category'];?></span>
-                <h3 class="lot__title"><a class="text-link" href="lot.html"><?=esc($lot['title']);?></a></h3>
+                <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($lot['title']);?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
                         <span class="lot__cost"><?=format_price($lot['price']);?></span>
                     </div>
                     <div class="lot__timer timer">
-
+                    <?=time_counter();?>
                     </div>
                 </div>
             </div>
