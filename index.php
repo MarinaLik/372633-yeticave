5999bab83a831e5cd19edcfc1c822156ebab9f3f
MarinaLik <mylym79@gmail.com>
2018-05-17 12:08:59 +0300

    Подключен файл bd_config

--- index.php
+++ index.php
@@ -1,33 +1,31 @@
 <?php
 require_once('functions.php');
+require_once('bd_config.php');
 
+// временно для пользователя
 $user = [
     'is_auth' => (bool) rand(0, 1),
     'user_name' => 'Константин',
     'user_avatar' => 'img/user.jpg'
 ];
 
-$link = mysqli_connect('localhost', 'irmida', 'qwerty', 'yeticave');
-mysqli_set_charset($link, "utf8");
-
 if (!$link){
-    print('Ошибка: ' . mysqli_connect_error());
+    print('Ошибка подключения: ' . mysqli_connect_error());
 }
 else {
     $sql = 'SELECT * FROM categories';
     $result_cat = mysqli_query($link, $sql);
 
     if (!$result_cat) {
-        $page_content = 'Произошла ошибка: ' . mysqli_error($link);
-        $categories = '';
+        print('Произошла ошибка: ' . mysqli_error($link));
     }
     else {
         $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
     }
-    $sql = 'SELECT date_start, date_end, title, price_start, image, categories.name FROM lots JOIN categories USING (category_id) WHERE date_end > NOW() ORDER BY date_start DESC';
+    $sql = 'SELECT lot_id, date_start, date_end, title, price_start, image, categories.name, COUNT(bet_id) AS amount_bet, MAX(bets.cost) AS max_price FROM lots JOIN categories USING (category_id) LEFT JOIN bets USING (lot_id) WHERE date_end > NOW() GROUP BY lot_id ORDER BY date_start DESC';
     $result_lot = mysqli_query($link, $sql);
     if (!$result_lot) {
-        $page_content = 'Произошла ошибка: ' . mysqli_error($link);
+        print('Произошла ошибка: ' . mysqli_error($link));
         }
     else {
         $lots = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
