451e528fe48e2d7b592c0c36fe4f61324f8765ab
MarinaLik <mylym79@gmail.com>
2018-05-18 16:13:56 +0300

    Добавлен показ шаблона с ошибкой, замена кода ответа сервера на 404

--- lot.php
+++ lot.php
@@ -1,16 +1,11 @@
 <?php
 require_once('functions.php');
 require_once('bd_config.php');
+require_once('data.php');
 
-// временно для пользователя
-$user = [
-    'is_auth' => (bool) rand(0, 1),
-    'user_name' => 'Константин',
-    'user_avatar' => 'img/user.jpg'
-];
-
-if (!$link) {
-    print('Ошибка подключения: ' . mysqli_connect_error());
+if (!$link){
+    $error = mysqli_connect_error();
+    $layout_content = include_template('error.php', ['error' => $error]);
 }
 else {
     $sql = 'SELECT * FROM categories';
@@ -19,42 +14,42 @@ else {
     if (!$result_cat) {
         print('Произошла ошибка: ' . mysqli_error($link));
     }
-    else {
-        $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
-    }
+    $categories = mysqli_fetch_all($result_cat, MYSQLI_ASSOC);
 
     if (isset($_GET['id'])) {
         $num_lot = intval($_GET['id']);
 
-        $sql = 'SELECT categories.name, title, description, image, date_start, date_end, price_start, rate_step, MAX(bets.cost) AS max_price FROM lots JOIN categories USING (category_id) LEFT JOIN bets USING (lot_id) WHERE lot_id = ' . $num_lot;
+        $sql = 'SELECT categories.name, title, description, image, date_start, date_end, price_start, rate_step FROM lots JOIN categories USING (category_id) LEFT JOIN bets USING (lot_id) WHERE lot_id = ' . $num_lot;
         $result_lot = mysqli_query($link, $sql);
+
         if (!$result_lot) {
-            print('Произошла ошибка: ' . mysqli_error($link));
+            $error = mysqli_error($link);
+            $page_content = include_template('error.php', ['error' => $error]);
             }
+        elseif (mysqli_num_rows($result_lot) < 1) {
+            http_response_code(404);
+            $error = 'Лот не найден';
+            $page_content = include_template('error.php', ['error' => $error]);
+        }
         else {
             $lot = mysqli_fetch_assoc($result_lot);
             $page_content = include_template('lot.php', [
             'categories' => $categories,
-            'lot' => $lot]);
+            'lot' => $lot,
+            'bets' => $bets]);
         }
-        if ($lot['title'] == NULL) {
-            http_response_code(404);
-            $page_content = 'Лот не найден';
-        }
-
     }
     else {
+        http_response_code(404);
         header("Location: /index.php");
     }
-
+    $layout_content = include_template('layout.php', [
+        'content' => $page_content,
+        'title' => $lot['title'],
+        'user' => $user,
+        'categories' => $categories
+    ]);
 }
-
-$layout_content = include_template('layout.php', [
-    'content' => $page_content,
-    'title' => $lot['title'],
-    'user' => $user,
-    'categories' => $categories
-]);
 
 print($layout_content);
 ?>
